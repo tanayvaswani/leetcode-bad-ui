896c0a62024f941ca21e4b2df9db6a49569bb96e
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-20 03:13:15 +0530

    feat: kachra bhar raha hu

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={cn("bg-neutral-900", inter.className)}>{children}</body>
     </html>
   );
 }
--- components/resizable/resizable-container.tsx
+++ components/resizable/resizable-container.tsx
@@ -6,32 +6,27 @@ import {
 
 export function ResizableDemo() {
   return (
-    <ResizablePanelGroup
-      direction="horizontal"
-      className="w-full rounded-lg border"
-    >
+    <ResizablePanelGroup direction="horizontal" className="w-full">
       <ResizablePanel defaultSize={50} minSize={3}>
-        <div className="bg-slate-100 flex h-full items-center justify-center py-2 pl-2 pr-1">
-          <span className="font-semibold bg-red-400 w-full h-full rounded-md">
+        <div className="flex h-full items-center justify-center py-2 pl-2 pr-1">
+          <span className="font-semibold bg-neutral-800 w-full h-full rounded-md">
             One
           </span>
         </div>
       </ResizablePanel>
-      <ResizableHandle />
 
       <ResizablePanel defaultSize={50} minSize={3}>
         <ResizablePanelGroup direction="vertical">
           <ResizablePanel defaultSize={94} minSize={6}>
-            <div className="flex bg-slate-100 h-full items-center justify-center pl-1 pb-1 pt-2 pr-2">
-              <span className="w-full h-full font-semibold bg-red-400 rounded-md">
+            <div className="flex h-full items-center justify-center pl-1 pb-1 pt-2 pr-2">
+              <span className="w-full h-full font-semibold bg-neutral-800 rounded-md">
                 Two
               </span>
             </div>
           </ResizablePanel>
-          <ResizableHandle />
           <ResizablePanel defaultSize={6} minSize={6}>
-            <div className="flex bg-slate-100 h-full items-center justify-center pl-1 pt-1 pb-2 pr-2">
-              <span className="w-full h-full font-semibold bg-red-400 rounded-md">
+            <div className="flex h-full items-center justify-center pl-1 pt-1 pb-2 pr-2">
+              <span className="w-full h-full font-semibold bg-neutral-800 rounded-md">
                 Three
               </span>
             </div>
